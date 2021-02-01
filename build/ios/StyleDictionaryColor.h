5e37453353bf68dfa15cd3ed80cd8f387b0dd24f
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-02-01 13:26:01 -0500

    Run style dictionary initialize command

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+// Do not edit directly
+// Generated on Sat, 30 Jan 2021 21:59:23 GMT
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Sat, 30 Jan 2021 21:59:24 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
