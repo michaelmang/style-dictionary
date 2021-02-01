09434ecd507676b541540e3eee1326d393e8f811
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-02-01 13:26:01 -0500

    Run style dictionary initialize command

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Sat, 30 Jan 2021 21:59:23 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
